bcf0679daae11b49cad5de30522d2e5df778fba3
VeraLimano <69342940+VeraLimano@users.noreply.github.com>
2021-09-07 09:54:32 +0300

    newChange

--- src/main/java/com/example/resttemplate/RestTemplateApplication.java
+++ src/main/java/com/example/resttemplate/RestTemplateApplication.java
@@ -15,7 +15,7 @@ public class RestTemplateApplication {
 
 	public static void main(String[] args) {
 
-		User user = new User(3L, "James", "Gray", (byte)26);
+		User user = new User(3L, "Вов", "Gray", (byte)26);
 		User user2 = new User(3L, "Thomas", "Shelby", (byte)26);
 
 		SpringApplication.run(RestTemplateApplication.class, args);
